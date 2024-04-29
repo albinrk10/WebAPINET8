0d7b573e398684b41372627ad4b25e38df029aac
Albin Anthony Hinostroza <79820950+albinrk10@users.noreply.github.com>
2024-04-28 21:01:32 -0500

    Agregar archivos de proyecto.

---
+++ WebAPINET8/Database/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace WebAPINET8.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Models.Customer> Customers { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            try
+            {
+               var dbCreaator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if (dbCreaator != null)
+                {
+                    if (!dbCreaator.CanConnect())
+                        dbCreaator.Create();
+                    if(!dbCreaator.HasTables())
+                        dbCreaator.CreateTables();
+
+                }
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+        }
+
+        
+    }
+}
